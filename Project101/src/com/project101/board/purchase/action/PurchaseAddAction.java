8fbab796d2ef8f4629f1d307b9d8b42a42afa2fb
user1 <user1@DESKTOP-BIBKLL9>
2018-05-25 15:11:09 +0900

    구매게시판 되는 거 : insert, update, delete 해야 할 일 : 이미지업로드, 댓글 , css, 로그인 연동

--- Project101/src/com/project101/board/purchase/action/PurchaseAddAction.java
+++ Project101/src/com/project101/board/purchase/action/PurchaseAddAction.java
@@ -1,5 +1,68 @@
 package com.project101.board.purchase.action;
 
-public class PurchaseAddAction {
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-}
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.project101.board.purchase.db.PurchaseBoardBean;
+import com.project101.board.purchase.db.PurchaseBoardDAO;
+
+public class PurchaseAddAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+
+		PurchaseBoardDAO buydao = new PurchaseBoardDAO();
+		PurchaseBoardBean boarddata = new PurchaseBoardBean();
+		ActionForward forward = new ActionForward();
+		
+		String realFolder="";
+		
+		String saveFolder="boardupload";
+		
+		int fileSize=5*1024*1024;
+		
+		ServletContext sc = request.getServletContext();
+		realFolder = sc.getRealPath(saveFolder);
+		
+		System.out.println("realFolder= " + realFolder);
+		boolean result = false;
+		
+		try {
+			MultipartRequest multi=null;
+			multi = new MultipartRequest(request, realFolder, fileSize, "utf-8", new DefaultFileRenamePolicy());
+			
+			boarddata.setId(multi.getParameter("id"));
+			boarddata.setTitle(multi.getParameter("title"));
+			boarddata.setContent(multi.getParameter("content"));
+			boarddata.setFile(multi.getFilesystemName((String)multi.getFileNames().nextElement()));			
+			result=buydao.PurchaseInsert(boarddata);
+
+			String fullpath=realFolder+"\\"+multi.getFilesystemName(multi.getFilesystemName((String)multi.getFileNames().nextElement()));
+			
+			System.out.println("fullpath=="+fullpath);
+			
+			if(result==false) {
+				System.out.println("게시판 등록 실패");
+				return null;
+			} else {
+				System.out.println("게시판 등록 완료");
+			}
+			
+
+			forward.setRedirect(true);
+			forward.setPath("./PurchaseListAction.buy");
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		return forward;
+		
+	}
+	}
--- Project101/src/com/project101/board/purchase/action/PurchaseModifyAction.java
+++ Project101/src/com/project101/board/purchase/action/PurchaseModifyAction.java
@@ -1,7 +1,5 @@
 package com.project101.board.purchase.action;
 
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,12 +9,14 @@ import com.project101.board.purchase.db.PurchaseBoardDAO;
 public class PurchaseModifyAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		request.setCharacterEncoding("euc-kr");
+	
 		ActionForward forward = new ActionForward();
-		
+		request.setCharacterEncoding("utf-8");
 		//글번호와 작성자를 가져온다.
 		int num = Integer.parseInt(request.getParameter("PB_NO"));
-		String id = request.getParameter("PB_WRITER");
+		
+		//로그인 연동 이후 수정
+		//String id = request.getParameter("PB_WRITER");
 		
 		boolean result = false;
 		
@@ -24,12 +24,14 @@ public class PurchaseModifyAction implements Action {
 		PurchaseBoardBean buydata = new PurchaseBoardBean();
 		
 		//글쓴이 인지 확인하기 위해 저장된 번호와 작성자를 확인합니다.
-		boolean idcheck = buydao.isBuyWriter(num, id);
+		
+		//로그인 연동 이후 수정
+		//boolean idcheck = buydao.isBuyWriter(num, id);
 		
 		//수정 내용을 설정합니다.
 		buydata.setNum(num);
 		buydata.setContent(request.getParameter("PB_CONTENT"));
-		buydata.setTitle(request.getParameter("board_content"));
+		buydata.setTitle(request.getParameter("PB_TITLE"));
 		
 		//DAO에서 수정 메서드 호출하여 수정합니다.
 		result = buydao.buyModify(buydata);
@@ -37,14 +39,14 @@ public class PurchaseModifyAction implements Action {
 		if(result == false) {
 			System.out.println("게시판 수정 실패");
 			return null;
-		}
+		} else {
 		//수정 성공의 경우
 		System.out.println("게시판 수정 완료");
-		
+		}
 		forward.setRedirect(true);
 		
 		//수정한 글 내용을 보여주기 위해 글 내용 보기 페이지로 이동하기 위해 경로를 설정함		
-		forward.setPath("./PurchaseListAction.buy?num=" + buydata.getNum()); //경로설정 변경 해야됨.
+		forward.setPath("./PurchaseDetailAction.buy?num=" + buydata.getNum()); //경로설정 변경 해야됨.
 		
 		return forward;
 	}
