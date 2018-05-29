b34e79ff9047a59dcfbc83268a79fcb819138637
user1 <user1@DESKTOP-BIBKLL9>
2018-05-29 17:21:14 +0900

    a

--- Project101/src/com/project101/member/action/FrontController.java
+++ Project101/src/com/project101/member/action/FrontController.java
@@ -46,7 +46,7 @@ public class FrontController extends HttpServlet {
 				forward = new ActionForward();
 				forward.setRedirect(false);
 				forward.setPath("/member/signup.jsp");
-			}else if(command.equals("/joinProcess.mem")) {
+			}else if(command.equals("/joinProcess.me")) {
 				action = new JoinProcessAction();
 				try {
 					forward=action.execute(request, response);
@@ -54,7 +54,7 @@ public class FrontController extends HttpServlet {
 				}catch(Exception e) {
 					e.printStackTrace();
 				}
-			}else if(command.equals("/idcheck.mem")) {
+			}else if(command.equals("/idcheck.me")) {
 				action = new IdCheckAction();
 				try {
 					forward=action.execute(request, response);
@@ -62,7 +62,7 @@ public class FrontController extends HttpServlet {
 				}catch(Exception e) {
 					e.printStackTrace();
 				}
-			}else if(command.equals("/emailCheck.mem")) {
+			}else if(command.equals("/emailCheck.me")) {
 				forward = new ActionForward();
 				forward.setRedirect(false);
 				forward.setPath("/member/emailCheck.jsp");
